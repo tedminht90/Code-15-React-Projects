b08f6d6f919b88c4ddde66365f8adc9d8c6bae10
tedminht90 <minhted@gmail.com>
2021-09-04 20:02:49 +0700

    Menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+
+const allCategories = ['all', ...new Set (items.map((item)=> item.category))];  //new set lọc những phần từ lặp lại chi trả về breakfast, lunch, shakes, thêm all
+//console.log(allCategories);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (categorys) => { //Lọc category trong menuItems bằng với categorys
+    if(categorys === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((menuItems) =>
+      menuItems.category === categorys)
+      setMenuItems(newItems);
+  };
+
+  return ( 
+  <main>
+    <section className= "menu section">
+      <div className="title">
+        <h2>Our menu</h2>
+        <div className="underline"></div>
+      </div>
+      {/* Truyền biến filterItems sang bên Categories.js */}
+      <Categories categories={categories} filterItems={filterItems}/> 
+      <Menu items={menuItems}/>
+    </section>
+  </main>
+  );}
 
 export default App;
