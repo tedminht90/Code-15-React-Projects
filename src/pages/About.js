3923359410cfb59203ff73e0cf6d88ca5200cae9
tedminht90 <minhted@gmail.com>
2021-09-18 14:27:39 +0700

    cocktails-getAPI

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel soluta voluptas, voluptatem temporibus cum impedit similique commodi obcaecati corporis eum. Illo voluptatibus quibusdam eveniet doloribus. Vero, veniam qui? Odio, molestiae.</p>
+    </section>
+  )
+}
+
+export default About
