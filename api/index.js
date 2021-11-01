eea1a846b0d23da1aa103d360e3f7d09ae72fb59
tedminht90 <minhted@gmail.com>
2021-11-01 11:58:46 +0700

    build netflix clone

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+dotenv.config();
+
+const URI = process.env.MONGO_URL;
+
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8866, () => {
+  console.log("Backend server is running");
+});
